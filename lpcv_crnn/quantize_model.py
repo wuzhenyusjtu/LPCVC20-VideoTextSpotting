1793bce6193a9796bf299f9c664ece5b024756de
yunhexue <44252675+yunhexue@users.noreply.github.com>
2020-11-13 17:28:59 -0600

    Update quantize_model.py

--- lpcv_crnn/quantize_model.py
+++ lpcv_crnn/quantize_model.py
@@ -7,6 +7,7 @@ import time
 import cv2
 import os
 import nltk
+import argparse
 
 import torch.nn as nn
 from torch.quantization import QuantStub, DeQuantStub
@@ -48,10 +49,10 @@ class BidirectionalLSTM(nn.Module):
         return output
 
 
-class CRNN(nn.Module):
+class CRNN_q(nn.Module):
 
     def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
-        super(CRNN, self).__init__()
+        super(CRNN_q, self).__init__()
         assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
 
         ks = [3, 3, 3, 3, 3, 3, 2]
@@ -100,7 +101,7 @@ class CRNN(nn.Module):
     def forward(self, input):
         # conv features
         input = self.cnn.quant(input)
-        print(input.dtype)
+        #print(input.dtype)
         for idx, m in enumerate(self.cnn.children()):
             if idx > 20:
                 break
@@ -122,10 +123,29 @@ class CRNN(nn.Module):
                                               ['conv3', 'relu3'], ['conv4', 'batchnorm4', 'relu4'], ['conv5', 'relu5'],\
                                               ['conv6', 'batchnorm6', 'relu6']], inplace=True)
 
-# model_path = '/data/yunhe/nni_crnn_finetune_pruned_l1_multi3_sample_all/netCRNN_1_100.pth'
-model_path = '/data/yunhe/nni_crnn_finetune_pruned_l1_multi3_sample_all_15/netCRNN_0.pth'
+parser = argparse.ArgumentParser()
+parser.add_argument('--pruned', required=True, help="path to pruned model (to continue training)")
+parser.add_argument('--expr_dir', required=True, help='Where to store samples and models')
+parser.add_argument('--imagedir', required=True, help='path to sample datasets')
+parser.add_argument('--imgH', type=int, default=32, help='the height of the input image to network')
+parser.add_argument('--imgW', type=int, default=100, help='the width of the input image to network')
+parser.add_argument('--nh', type=int, default=256, help='size of the lstm hidden state')
+# TODO(meijieru): epoch -> iter
+parser.add_argument('--alphabet', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz')
+parser.add_argument('--qconfig', default='fbgemm', help="type of quantization configure (fbgemm or qnnpack)")
+parser.add_argument('--manualSeed', type=int, default=1234, help='reproduce experiemnt')
+parser.add_argument('--random_sample', action='store_true', help='whether to sample the dataset with random sampler')
+opt = parser.parse_args()
+opt.cuda = True
+opt.adadelta = True
+print(opt)
+
+model_path = opt.pruned
 img_path = './data/demo.png'
 alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+if opt.qconfig not in ['fbgemm', 'qnnpack']:
+    raise NameError('qconfig parameter should be qnnpack or fbgemm')
 
 def load_multi(model_path):
     state_dict = torch.load(model_path, map_location='cpu')
@@ -139,12 +159,9 @@ def load_multi(model_path):
         new_state_dict[name] = v
     return new_state_dict
 
-model = CRNN(32, 1, 37, 256)
+model = CRNN_q(32, 1, 37, 256)
 model.eval()
-# print(model)
 model = model.cpu()
-# print('loading pretrained model from %s' % model_path)
-# model.load_state_dict(torch.load(model_path))
 model.load_state_dict(load_multi(model_path), strict=False)
 
 for idx, m in enumerate(model.rnn.children()):
@@ -153,28 +170,22 @@ for idx, m in enumerate(model.rnn.children()):
 model.fuse_model()
 print(model)
 
-qconf = torch.quantization.get_default_qconfig('fbgemm')
+qconf = torch.quantization.get_default_qconfig(opt.qconfig)
 model.cnn.qconfig = qconf
 for idx, m in enumerate(model.rnn.children()):
     m.embedding_w.qconfig = qconf
-# model.rnn.qconfig = qconf
-#     model.proposal_generator.fcos_head.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+
 torch.quantization.prepare(model.cnn, inplace=True)
 for idx, m in enumerate(model.rnn.children()):
     torch.quantization.prepare(m.embedding_w, inplace=True)
-# torch.quantization.prepare(model.rnn, inplace=True)
 
 print(model)
 
 converter = utils.strLabelConverter(alphabet)
 transformer = dataset.resizeNormalize((100, 32))
 image = cv2.imread(img_path)
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# image = Image.open(img_path).convert('L')
 image = transformer(image)
-# if torch.cuda.is_available():
-#     image = image.cuda()
 image = image.view(1, *image.size())
 image = Variable(image)
 preds = model(image)
@@ -185,7 +196,7 @@ preds = preds.transpose(1, 0).contiguous().view(-1)
 preds_size = Variable(torch.IntTensor([preds.size(0)]))
 raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
 sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
-print('%-20s => %-20s' % (raw_pred, sim_pred))
+# print('%-20s => %-20s' % (raw_pred, sim_pred))
 
 def get_crnn_res(model, image_dir, converter, transformer):
     total_dist = 0
@@ -202,8 +213,6 @@ def get_crnn_res(model, image_dir, converter, transformer):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         
         text_patch = transformer(image)
-#         if torch.cuda.is_available():
-#             text_patch = text_patch.cuda()
         text_patch = text_patch.view(1, *text_patch.size())
         text_patch = Variable(text_patch)  
         #     print(text_patch.shape)
@@ -215,21 +224,21 @@ def get_crnn_res(model, image_dir, converter, transformer):
         raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
         sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
         edit_distance = nltk.edit_distance(sim_pred.upper(), label.upper())
-        print('{} => {}, gt label:{}, e_dist:{}'.format(raw_pred, sim_pred, label, edit_distance))
+#         print('{} => {}, gt label:{}, e_dist:{}'.format(raw_pred, sim_pred, label, edit_distance))
         total_dist += edit_distance
         total_cnt += 1
         
     return total_dist, total_cnt
 
-image_dir = './train_dataset_bezier'
+image_dir = opt.imagedir
 get_crnn_res(model, image_dir, converter, transformer)
 
 torch.quantization.convert(model.cnn, inplace=True)
 for idx, m in enumerate(model.rnn.children()):
     torch.quantization.convert(m.embedding_w, inplace=True)
-#     torch.quantization.convert(m.quant, inplace=True)
 print(model)
-torch.jit.save(torch.jit.script(model.cnn), './cnn_final_15_qnnpack.torchscript')
+torch.jit.save(torch.jit.script(model.cnn), '{}/cnn_CRNN_{}.torchscript'.format(opt.expr_dir, opt.qconfig))
+print("cnn part is saved to {}/cnn_CRNN_{}.torchscript".format(opt.expr_dir, opt.qconfig))
 for idx, m in enumerate(model.rnn.children()):
-    torch.jit.save(torch.jit.script(m.embedding_w), './crnn_embd_w{}_final_15_qnnpack.torchscript'.format(idx))
-#     torch.jit.save(torch.jit.script(m.quant), './test_crnn_all_quant.torchscript')
+    torch.jit.save(torch.jit.script(m.embedding_w), '{}/embd_w{}_CRNN_{}.torchscript'.format(opt.expr_dir, idx, opt.qconfig))
+    print("embedding part {} is saved to {}/embd_w{}_CRNN_{}.torchscript".format(idx,opt.expr_dir, idx, opt.qconfig ))
