42e4425f16633b726b55b93344d59863dd953a41
yunhexue <44252675+yunhexue@users.noreply.github.com>
2020-11-29 14:28:00 -0600

    Update get_data_json.py

--- lpcv_crnn/get_data_json.py
+++ lpcv_crnn/get_data_json.py
@@ -4,54 +4,53 @@ import random
 import argparse
 import json
 
+'''
+Get new train/test lists from original one
+
+Args:
+    txt_file: path to the annotation txt file of the original mjdataset
+    num: number of trainging/test images you want to use
+    prob: probability to continue
+    
+Return:
+    A list contains the paths to the images/data you select. 
+    The length of the list is the same with the number parameter.
+'''
+def get_selected_list(txt_file, num, prob):
+    path_list = []
+    cnt = 0
+    with open(txt_file, 'r', encoding='ascii') as f:
+        line = f.readline() 
+        while line: 
+            lines = line.split(' ')
+            t = random.randint(0, 100)
+            if t <= 100*prob:
+                line = f.readline() 
+                continue
+            cnt += 1
+            if cnt >= num:
+                break
+            path_list.append(lines[0][2:])
+
+            line = f.readline() 
+    return path_list
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', required=True, help='path to dataset')
     parser.add_argument('--train_num', required=True, help='train data number')
     parser.add_argument('--test_num', required=True, help='test data number')
+    parser.add_argument('--prob', default=0.5, help='probability for data selection method to continue')
     opt = parser.parse_args()
-    
-    mjdata_dir = opt.datadir
-
-    img_pathlist_train = []
-    label_list_train = []
-    cnt = 0
-    with open(os.path.join(mjdata_dir,'annotation_train.txt'), 'r', encoding='ascii') as f:
-        line = f.readline() 
-        while line: 
-
-            lines = line.split(' ')
-            t = random.randint(0, 100)
-            if t <= 50:
-                line = f.readline() 
-                continue
-            cnt += 1
-            if cnt >= int(opt.train_num):
-                break
-            img_pathlist_train.append(lines[0][2:])
-
-            line = f.readline() 
-            
-    img_pathlist_test = []
-    cnt = 0
-    with open(os.path.join(mjdata_dir,'annotation_test.txt'), 'r', encoding='ascii') as f:
-        line = f.readline() 
-        while line: 
-
-            lines = line.split(' ')
-            t = random.randint(0, 100)
-            if t <= 50:
-                line = f.readline() 
-                continue
-            cnt += 1
-            if cnt >= int(opt.test_num):
-                break
-            img_pathlist_test.append(lines[0][2:])
-            line = f.readline() 
-
-    with open(os.path.join(opt.datadir, 'test_new.json'), 'w') as file:
-        json.dump(img_pathlist_test, file)
+        
+    img_pathlist_train = get_selected_list(os.path.join(opt.datadir,'annotation_train.txt'), int(opt.train_num), float(opt.prob))
+    img_pathlist_test = get_selected_list(os.path.join(opt.datadir,'annotation_test.txt'), int(opt.test_num), float(opt.prob))
+#     print(len(img_pathlist_train))
+#     print(len(img_pathlist_test))
     with open(os.path.join(opt.datadir,'train_new.json'), 'w') as file:
         json.dump(img_pathlist_train, file)
+    with open(os.path.join(opt.datadir, 'test_new.json'), 'w') as file:
+        json.dump(img_pathlist_test, file)
+
 if __name__ == '__main__':
     main()
